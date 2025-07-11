292d18ab508ae91f2570deb923e2338680bb15cb
flokill751 <flokill751@gmail.com>
2025-07-11 10:06:51 -0300

    Estrutura multi-modal maven criada

---
+++ modulo-basico/src/main/java/SaidaDeDados/saidaAtividadeMain.java
@@ -0,0 +1,31 @@
+package SaidaDeDados;
+
+import java.util.Locale;
+
+public class saidaAtividadeMain {
+    public void main(String[] args) {
+
+
+                String produto1 = "Computador";
+                String produto2 = "Mesa de Escritório";
+
+                int idade = 45;
+                int code = 7894;
+                char genero = 'F';
+
+                double preco1 = 5852.0;
+                double preco2 = 785.50;
+                double medida = 53.002345;
+
+
+                System.out.printf("Produtos:%n%s, tem um preço de %f%n", produto1, preco1);
+                System.out.printf("%s, tem um preço de %f%n", produto2, preco2);
+                System.out.printf("Recorde: %d ano, codigo: %d e genero: F%n ", idade, code, genero );
+                System.out.printf("Medida em 8 decimais: %f%n Rouded (medida em tres decimais): %.3f%n", medida, medida);
+                Locale.setDefault(Locale.US);
+                System.out.printf("US decimal ponto: %.3f%n", medida);
+
+
+
+    }
+}
---
+++ modulo-basico/src/main/java/SaidaDeDados/saidaMain.java
@@ -0,0 +1,26 @@
+package SaidaDeDados;
+
+import java.util.Locale;
+
+public class saidaMain {
+
+    public static void main(String[] args) {
+
+
+        double Africano = 520.46544899846546498894968489489489498944;
+        String nome = "Mario Dementes";
+        int idade = 48;
+        double renda = 10000.0;
+
+
+        System.out.println("Cachorro");
+        System.out.printf("%.2f%n", Africano);
+        System.out.println("Cachorro fumou o " + Africano + " TANOS de maconha");
+        System.out.printf("Fumaram %.2f TANOS de maconha%n", Africano);
+        System.out.printf("%s tem %d anus e ganha R$ %.4f reais%n", nome, idade, renda);
+        Locale.setDefault(Locale.US);
+        System.out.print(Africano);
+
+
+        }
+    }
