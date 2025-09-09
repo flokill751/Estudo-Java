f9a4a8b2a5674e530cf334b2f0a069ebedb16a01
flokill751 <flokill751@gmail.com>
2025-09-09 20:28:17 +0000

    Estrutura FOR atualizada

--- modulo-basico/src/main/java/estruturaRepetitivas/EstruturaRepetitivaFor.java
+++ modulo-basico/src/main/java/estruturaRepetitivas/EstruturaRepetitivaFor.java
@@ -1,7 +1,21 @@
 package estruturaRepetitivas;
 
+import java.util.Scanner;
+
 public class EstruturaRepetitivaFor {
     public static void main(String [] args){
 
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int soma = 0;
+
+        for (int i = 0; i<N; i++){
+
+            int x = sc.nextInt();
+            soma = soma + x;
+        }
+        System.out.println(soma);
     }
 }
